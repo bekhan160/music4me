35c693f871b594dc972300a3374c0ab8d7a15f8f
bekhan160 <59920301+bekhan160@users.noreply.github.com>
2022-06-22 19:39:50 +0300

    Update music4me2022.py

--- music4me2022.py
+++ music4me2022.py
@@ -7,7 +7,7 @@ import ffmpeg
 
 
 
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'False', 'simulate': 'True',
+YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist': 'False', 'simulate': 'True',
                'preferredquality': '192', 'preferredcodec': 'mp3', 'key': 'FFmpegExtractAudio'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
